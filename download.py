f7c253a6305d0477f286b08a602a3f6d052a1d7a
chtzs <wsbcht@sina.com>
2022-09-13 22:58:22 +0800

    Polish

--- download.py
+++ download.py
@@ -1,5 +1,4 @@
 import requests
-import re
 import json
 import datetime
 import load_cookies
--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+import os
 from openpyxl import Workbook
 from download import SheetDownloader
 from sheet_generator import SheetGenerator
 
+from sys import argv
+
+USAGE = """
+USAGE: python3 main.py <tencent document url> [cookie file]
+"""
+
+def try_get_url():
+    if len(argv) == 1:
+        print(USAGE)
+        exit(1)
+    else:
+        url = argv[1]
+        return url
 
 def main():
-    url = "https://docs.qq.com/sheet/DVGh0WlNHTmNCUlNH?tab=4hutl0"
+    url = try_get_url()
     downloader = SheetDownloader(url)
     print("Document name: %s" % downloader.title)
     wb = Workbook()
     for tab in downloader.tabs:
-        print("Fetching %s..." % tab["name"])
+        print("Fetching sheet %s..." % tab["name"])
         content, _, max_col = downloader.fetch_sheet_data(tab["id"])
         _ = SheetGenerator(wb.create_sheet(tab["name"]), content, max_col)
+
+    print("Generating...")
 
     empty_ws = wb["Sheet"]
     wb.remove(empty_ws)
     wb.save("%s.xlsx" % downloader.title)
     print("Saved to %s.xlsx" % downloader.title)
+
 
 if __name__ == "__main__":
     main()
